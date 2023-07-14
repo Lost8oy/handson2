01ce1fe0d15d2bdb3ab4ac713aa8cd8501865097
Gianmarco Paffetti <gianmarcopaffetti@gmail.com>
2023-07-14 09:49:58 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,7 @@ route::get('/', function() {
     return view('welcome');
 });
 
-route::get('/countries', [CountryController::class, 'read'])->name('country.read');
-route::get('/countries/create', [CountryController::class, 'create'])->name('country.create');
 route::get('/manufacturers', [ManufactureController::class, 'read'])->name('manufacturer.read');
 route::get('/manufacturers', [ManufactureController::class, 'create'])->name('manufacturer.create');
 
 
-route::get('/computers/{id}', function($id) {
-    return view('computer', [
-        'computer' => computer::find($id)
-    ]);
-});
