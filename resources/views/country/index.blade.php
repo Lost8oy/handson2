e9eb82d276bf625c11c50d8eb14dbea2bfdb256f
Achi Romano <lostboy404@icloud.com>
2023-07-14 10:01:41 +0200

    hi

--- resources/views/country/index.blade.php
+++ resources/views/country/index.blade.php
@@ -8,7 +8,44 @@
 </head>
 <body>
     <h1>Index</h1>
-    <div>computers</div>
+    <div>
+        @if(session()->has('success'))
+        <div>
+            {{session('success')}}
+        </div>
+        @endif
+    </div>
+    <div>
+        <div>
+            <a href="{{route('country.create')}}">Create a Country</a>
+        </div>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+            @foreach ($countries as $country)
+            <tr>
+                <td>{{$country->id}}</td>
+                <td>{{$country->name}}</td>
+                <td>{{$country->description}}</td>
+                <td>
+                    <a href="{{route('country.edit', ['country' => $country])}}">Edit</a>
+                </td>
+                <td>
+                    <form method="post" action="{{route('country.destroy', ['country' => $country])}}">
+                        @csrf
+                        @method('delete')
+                        <input type="submit" value="Delete" />
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
     
 </body>
 </html>
